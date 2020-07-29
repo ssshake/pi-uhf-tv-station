fbeafb8dc8f876ec7aa6e3ec616f8f5df80659cb
Your Name <you@example.com>
2020-07-30 00:53:18 +0100

    broke something

--- api/api.js
+++ api/api.js
@@ -90,42 +90,60 @@ app.get('/voldown', (req, res) => {
 	res.json({ nowPlaying: currentVideo});
 });
 
-app.get('/chdown', (req, res) => {
-	player.volDown();
+app.get('/chup', (req, res) => {
+	channelUp();
 	res.json({ nowPlaying: currentVideo});
 });
 app.get('/chdown', (req, res) => {
-	player.volDown();
+	channelDown();
 	res.json({ nowPlaying: currentVideo});
 });
 
 console.log('Starting Pi TV Station');
 
-const videoPath = "/media/video/TV/Star Trek TNG/Star.Trek.The.Next.Generation.S01.NTSC.DVD.DD5.1.x264-JCH/";
-
-const shows = [
-	"Sonic",
+const basePath = "/media/video/TV/"
+const playlists = [
 	"Star Trek TNG/Star.Trek.The.Next.Generation.S01.NTSC.DVD.DD5.1.x264-JCH/",
+	"Sonic",
+	"Super Mario Bros/Super Mario Bros Super Show Vol2/SMBSS2 Disk 1",
+	"Super Mario Bros/Super Mario Brothers 3",
+	"Legend of Zelda",
+	"Cosmos/Season 1",
+	"Sliders/Sliders full/Season 1",
+	"The Real Ghostbusters - Season 1-7/Season 1 - 13 eps - dvdrip - mer-der",
+	"Cosmos Original",
+	"Duckman",
+	"ReBoot/Season 1",
+	"Futurama/Futurama.COMPLETE.DVDRip.MiXED/Season Two",
 ]
 
 let prevEpisodes = [];
 let episodes = [];
 let currentVideo = "";
+let currentPlaylistIndex = 0;
+let videoPath = "";
 
-fs.readdir(videoPath, function(err, files){
-	if (err) {
-		return console.log('unable to scan dir ' + err);
-	}
 
-	episodes = files.reverse();
-	
-	//files.forEach(function(file) {
-	//	console.log(file);
-	//	episodes.push(file);
-	//});
+	videoPath = basePath + playlists[currentPlaylistIndex];
 
-	playNextVideo();
-});
+	fs.readdir(videoPath, function(err, files){
+		if (err) {
+			return console.log('unable to scan dir ' + err);
+		}
+
+		episodes = files.reverse();
+		playNextVideo();
+	});
+
+const channelUp = () => {
+	currentPlaylistIndex = (currentPlaylistIndex + 1) % playlists.length;
+	loadPlaylist();
+}
+
+const channelDown = () => {
+	currentPlaylistIndex = (currentPlaylistIndex - 1) % playlists.length;
+	loadPlaylist();
+}
 
 const playNextVideo = () => {
 	if (episodes.length <= 0){
@@ -133,8 +151,10 @@ const playNextVideo = () => {
 		return;
 	}
 	let nextVideo = episodes.pop();
+
 	console.log("Play Next Video");
 	console.log(nextVideo);
+
 	player.newSource(videoPath + nextVideo);
 	if (currentVideo.length > 0){
 		prevEpisodes.push(currentVideo);
@@ -148,18 +168,22 @@ const playPrevVideo = () => {
 		return;
 	}
 	let prevVideo = prevEpisodes.pop();
+
 	console.log("Play Prev Video");
 	console.log(prevVideo);
+
 	player.newSource(videoPath + prevVideo);
         episodes.push(currentVideo);
 	currentVideo = prevVideo;
 }
 
 let player = Omx();
+player.info();
+loadPlaylist();
+
 app.listen(port, () => {
 	console.log("tv station up on " + port);
 });
 
 
-Super Mario Bros/Super Mario Bros Super Show Vol2/SMBSS2 Disk 1
-Super Mario Bros/Super Mario Brothers 3
+
