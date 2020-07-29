a34d21b12f71fd84be2eea30217c64f48f5f55b6
Richard Bettridge <daggasoft@gmail.com>
2020-07-29 14:29:05 -0400

    wood grain

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RemoteControl/>
+  </div>
+</template>
+
+<script>
+import RemoteControl from './components/RemoteControl.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RemoteControl
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/RemoteControl.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="remote">
+    Now Playing: {{ nowplaying }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RemoteControl',
+  data: () => {
+    return {
+      baseUrl: 'http://10.0.0.20:3000',
+      nowplaying: '',
+    };
+  },
+  methods: {
+    nowPlaying(){
+      fetch(`${this.baseUrl}/nowplaying`)
+      .then(response => response.text())
+      .then((data) => {
+        console.log(data)
+        this.nowplaying = JSON.parse(data).nowPlaying
+      }) 
+    }
+  },
+  mounted(){
+    this.nowPlaying();
+  }
+}
+</script>
+
+
+<style scoped>
+  .remote {
+    background-image: url('../assets/woodgrain.jpg')
+  }
+</style>
