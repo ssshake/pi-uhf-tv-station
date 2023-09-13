8f251d889ad04c26dcc3fb67b7baac3aa7befb81
Your Name <you@example.com>
2023-09-13 21:29:01 +0100

    adds shuffle button to remote

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBackward, faFastBackward, faForward, faFastForward, faPlay, faPause, faStop, faEject, 
+import { faQuestion, faBackward, faFastBackward, faForward, faFastForward, faPlay, faPause, faStop, faEject, 
   faPowerOff, faVolumeUp, faVolumeDown, faVolumeMute, faSortUp, faSortDown, faCircle, faRetweet } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -23,6 +23,7 @@ library.add(faSortUp)
 library.add(faSortDown)
 library.add(faCircle)
 library.add(faRetweet)
+library.add(faQuestion)
 
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
